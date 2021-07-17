0f9bc8b17c6e4d0c6943dedca6a7862ce91cbd5d
Denis Singh <denis.darkrgd@gmail.com>
2021-07-17 15:50:46 +0300

    Added all styles

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,22 +4,31 @@ import Section from 'components/Section';
 import s from './Contacts.module.css';
 
 const Contacts = ({ title, contacts, buttonDelete }) => {
+  console.log(contacts.length === 0);
   return (
     <Section title={title}>
-      <ul className={s.list}>
-        {contacts.map(contact => {
-          const { id, name, number } = contact;
+      {contacts.length !== 0 ? (
+        <ul className={s.list}>
+          {contacts.map(contact => {
+            const { id, name, number } = contact;
 
-          return (
-            <li className={s.item} key={id}>
-              {name}: {number}
-              <button type="button" onClick={() => buttonDelete(id)}>
-                Delete
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+            return (
+              <li className={s.item} key={id}>
+                {name}: {number}
+                <button
+                  className={s.button}
+                  type="button"
+                  onClick={() => buttonDelete(id)}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p className={s.notification}>You don't have a contacts yet :(</p>
+      )}
     </Section>
   );
 };
